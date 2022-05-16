836a2de9ca798742598c5ec77efd857d53eb15c8
SumanVastrakar <96719499+SumanVastrakar@users.noreply.github.com>
2022-05-16 22:51:41 +0530

    First Day assignment completed

---
+++ src/components/UserInfo.js
@@ -0,0 +1,38 @@
+export const userInfo = [
+    {
+        id : 1,
+        imgUrl : "",
+        name : "Annette Murphy",
+        email :  "Annette@gmail.com"
+    },
+    {
+        id : 2,
+        imgUrl : "",
+        name : "Theresa Watson",
+        email :  "Theresa@gmail.com"
+    },
+    {
+        id : 3,
+        imgUrl : "",
+        name : "Cody Howard",
+        email :  "Cody@gmail.com"
+    },
+    {
+        id : 4,
+        imgUrl : "",
+        name : "Max Lane",
+        email :  "Max@gmail.com"
+    },
+    {
+        id : 5,
+        imgUrl : "",
+        name : "Marvin Fisher",
+        email :  "Marvin@gmail.com"
+    },
+    {
+        id : 6,
+        imgUrl : "",
+        name : "Brookiyn Mccoy",
+        email :  "Brookiyn@gmail.com"
+    },
+]
